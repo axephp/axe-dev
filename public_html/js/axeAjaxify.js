dd0fa150ecd65bb4b81f84bd429cb40deb5f7f60
Jeevan Prakash Pant <jeevanppant@gmail.com>
2017-04-26 00:34:13 +0530

    axe js plugin

---
+++ public_html/js/axeAjaxify.js
@@ -0,0 +1,123 @@
+function axeAjaxify(infoz){
+
+	var frmId = infoz.id;
+	var onsuccess = infoz.success;
+	var valUrl = infoz.validate;
+
+
+	var inp = $('#' + frmId + ' :input');
+	
+	
+	inp.click(function(event){
+		if ($(this).attr('type') == "radio" || $(this).attr('type' == "checkbox")) {
+			var id = $(this).attr('id');
+	    	getInfo(id, $('#'+id).val(), status, function(){});
+	    }
+    });
+
+	inp.on("input", function(event){
+		var id = $(this).attr('id');
+        getInfo(id, $('#'+id).val(), function(){});
+    });
+
+
+    $( "#" + frmId).submit(function( event ) {
+
+	  	// Get all the inputs into an array...
+         var inputs = $('#form :input');
+
+         var status = true;
+         inputs.each(function (index)
+         {
+             if(!($(this).attr('id') == "submit" || $(this).attr('id') == null))
+             {
+             	
+				var id = $(this).attr('id');
+				var value = $(this).val();
+				getInfo(id, value, function(sts){
+
+				});
+
+	        	 if(!$('#' + id + '_info').hasClass("valid"))
+	             {
+	             	status = false;
+	             }
+             }
+         });
+
+
+        if (status) {
+	         submitInfo($(this), onsuccess);
+		}else{
+			alert("Please make sure you have all correct inputs.");
+		}
+
+		event.preventDefault();
+	});
+
+
+	function getInfo(field, info, returned){
+		var a = $.ajax({
+			method: "GET",
+			url: valUrl, 
+			data: {[field]: info},
+	        success: function(msg) {
+	            // Call this function on success
+	        	ret =  doneAction( msg, field );
+	        	returned(ret);
+
+	        },
+	        error: function() {
+	            alert('Error occured');
+	        }
+		});
+
+		return a;
+
+		 //alert(returned);
+	}
+
+	function submitInfo(form, successed){
+
+		var data = JSON.stringify( form.serializeArray());
+
+		//alert(data);
+
+		var action = $(form).attr('action');
+		var method = $(form).attr('method');
+
+	    $.ajax({
+	        url: action,
+	        type: method,
+	        data: data,
+	        success: function(response) {
+	           successed(response);
+	        }            
+	    });
+	}
+
+	function doneAction(msg, field)
+	{
+		if (msg.error == "phperror") {
+			alert(msg.message);
+		}
+		
+		//console.log(JSON.stringify(msg));
+
+		if (msg[field].status == 'success') {
+			$('#'+field+'_info').html(createMessage('#558B2F', 'check-circle'));
+			$('#'+field+'_info').addClass('valid');
+			return true;
+		}else{
+			$('#'+field+'_info').html(createMessage('#FF3333', 'times-circle', msg[field].message));
+			$('#'+field+'_info').removeClass("valid");
+			return false;
+		}
+		//console.log("Returned: " + this.msg.info);
+	}
+
+	function createMessage(color, icon, message = '') {
+		return '<div style="color:'+color+';"><i class="fa fa-'+icon+' fa-lg"></i> '+message+'</div>';
+	}
+
+}
